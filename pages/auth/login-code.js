66a95a6efa260fd9de5e971cc1a33bb87884dc09
wushuyi <wushuyi541@gmail.com>
2017-11-14 17:53:56 +0800

    add auth

---
+++ pages/auth/login-code.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Layout from 'components/layout/default';
+import {withRedux} from 'store';
+import logic from 'pagelogic/noop';
+import {connect} from 'libs/kea';
+import {isBrowser} from 'utils';
+import LoginByCode from 'components/pages/loginByCode';
+import getDB from 'utils/getDB';
+
+@connect({
+  actions: [
+    logic, []
+  ],
+})
+class Index extends React.Component {
+  static childContextTypes = {
+    url: PropTypes.any
+  };
+
+  constructor() {
+    super();
+    this.libs = {};
+  }
+
+  static async getInitialProps({isServer, store, req, actions, selectors}) {
+    let jquery, props = {};
+    // store.dispatch(logic.actions.increment(2));
+    return props;
+  }
+
+  getChildContext() {
+    let {url} = this.props;
+    return {
+      url
+    };
+  }
+
+  async componentDidMount() {
+    let db = await getDB();
+    window.db = db;
+    console.log(db.getCollection('users'))
+  }
+
+
+  render() {
+
+    return (
+      <Layout>
+        <LoginByCode/>
+      </Layout>
+    );
+  }
+}
+
+export default withRedux(Index);
